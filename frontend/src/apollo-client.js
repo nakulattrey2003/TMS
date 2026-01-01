3fb1253928e7193174d3184ac88b54e9db709810
Nakul Attrey <nakulattrey@gmail.com>
2026-01-01 22:50:17 +0530

    Refactor Apollo Client configuration and add .env.example for GraphQL URL

--- frontend/src/apollo-client.js
+++ frontend/src/apollo-client.js
@@ -1,15 +1,21 @@
-import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloLink,
+} from "@apollo/client";
 
+// Read GraphQL endpoint from Vite env var (must start with VITE_ to be exposed to client)
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_URL || "http://localhost:4000/graphql",
 });
 
-const authMiddleware = new ApolloLink(function(operation, forward) {
-  const token = localStorage.getItem('token');
+const authMiddleware = new ApolloLink(function (operation, forward) {
+  const token = localStorage.getItem("token");
   operation.setContext({
     headers: {
-      authorization: token ? `Bearer ${token}` : '',
-    }
+      authorization: token ? `Bearer ${token}` : "",
+    },
   });
   return forward(operation);
 });
